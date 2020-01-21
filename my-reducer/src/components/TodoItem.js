8e01c328b33a0c837295cf240950e4b3c5b36e03
qb1968 <you@example.com>
2020-01-21 17:26:31 -0500

    final

--- my-reducer/src/components/TodoItem.js
+++ my-reducer/src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const TodoItem = ({ todo, dispatch }) => {
+    
     return (
         <div className='todo-item'>
             <span className='item' style={todo.completed ? {textDecoration: 'line-through'} : {}} onClick={() => dispatch({type: 'toggle', payload: todo.id})}>{todo.item}</span>
